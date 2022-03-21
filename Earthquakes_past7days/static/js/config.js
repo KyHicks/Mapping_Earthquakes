0c952298ca4d09485847099d1ece8bf9be674061
KyHicks <kyliehicks76@yahoo.com>
2022-03-21 10:34:57 -0500

    Add legend to earthquake map

--- Earthquakes_past7days/static/js/config.js
+++ Earthquakes_past7days/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_Key = "pk.eyJ1Ijoia3loaWNrcyIsImEiOiJjbDByN2tuNTMwMHN5M2lrM2hwcjBhNmRtIn0.qxcawCc87eRWbQUYOgZSSg";
+const API_KEY = "pk.eyJ1Ijoia3loaWNrcyIsImEiOiJjbDByN2tuNTMwMHN5M2lrM2hwcjBhNmRtIn0.qxcawCc87eRWbQUYOgZSSg";
